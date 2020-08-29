250b21e645361d4f7e03952035cc284da50e4fe1
QuixoteHY <quixotehy@qq.com>
2020-08-29 13:36:45 +0800

    add tushare

---
+++ code/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# @Time     : 2020/8/29 13:02
+# @Author   : 胡远
+# @Github   : https://github.com/QuixoteHY
+# @Email    : quixotehy@qq.com
+# @Describe :
---
+++ code/data_source/tushare/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @Time     : 2020/8/29 13:02
+# @Author   : 胡远
+# @Github   : https://github.com/QuixoteHY
+# @Email    : quixotehy@qq.com
+# @Describe :
+
+import tushare
+
+
+tushare.set_token('1e431dd1d92959eeec4ef91f58a3ec1f85b5b242d32f1c3a2b00df08')
+tushare_client = tushare.pro_api()
+
