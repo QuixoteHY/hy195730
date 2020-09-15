b585521d36606559cc10dce8e12fa3029325cff9
QuixoteHY <quixotehy@qq.com>
2020-09-15 18:21:05 +0800

    commit

--- code/data_source/tushare/download_tick_data.py
+++ code/data_source/tushare/download_tick_data.py
@@ -13,7 +13,7 @@ def run(config, exchange='', start_date='2018-12-12', end_date=''):
     # df = tushare_client.get_tick_data('600848', date=start_date, src='tt')
     df = tushare.get_tick_data('600848', date=start_date, src='tt')
     print(df.head(10))
-    df.to_csv('G:\hy195730\data\df1.csv')
+    df.to_csv('G:\hy195730\data\df2.csv')
 
 
 if __name__ == '__main__':
